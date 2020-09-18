aeaf8a225607c5bbceb114482a89da81ce31c82b
Anirudha-CA <71438282+Anirudha-CA@users.noreply.github.com>
2020-09-18 15:03:25 +0530

    Add files via upload

---
+++ nestfor.java
@@ -0,0 +1,13 @@
+public class nestfor {
+    public static void main(String[] args){
+        int i,j,n;
+        for (i=5;i>=1;i--)
+        {
+            for (j=5;j>=1;j--)
+            {
+                System.out.println(j);
+            }
+            System.out.println("");
+        }
+    }
+}
