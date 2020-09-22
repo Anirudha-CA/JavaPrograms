1059a77344fb238a4bd9468babe067c624706510
Anirudha-CA <71438282+Anirudha-CA@users.noreply.github.com>
2020-09-22 21:35:39 +0530

    method overloading

---
+++ cons.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class cons {
+    cons(int roll)
+    {
+        System.out.println("Name: Unknown");
+        System.out.println("Roll number: "+roll);
+    }
+    cons(String name,int roll)
+    {
+        System.out.println("Name: "+name);
+        System.out.println("Roll Number: "+roll);
+    }
+    public static void main(String[] args){
+        int rolln;
+        String names;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the name and roll number");
+        names = sc.nextLine();
+        rolln = sc.nextInt();
+        cons s1 = new cons(names,rolln);
+        cons s2 = new cons(rolln);
+
+
+    }
+
+}
